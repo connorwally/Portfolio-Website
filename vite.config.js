ad8a10702e8d4ceab97010f4d28ae37b04f7b3fc
Connor <wally.connor17@gmail.com>
2023-12-24 22:47:42 +0800

    asd

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/portfolio-website/",
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
 });
